136e5abcea425d4c33ccc564575b4dea54477949
Sam Edwards <samedwards@dropbox.com>
2022-04-24 15:05:07 -0400

    Removing extra gradle wrapper.

--- dependency-guard-gradle-plugin/settings.gradle.kts
+++ dependency-guard-gradle-plugin/settings.gradle.kts
@@ -1,17 +1,12 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 rootProject.name = "dependency-guard-gradle-plugin"
 
+// Because this is pulled in via "includeBuild" from the root project,
+// we need to point to the version catalog.  This same method is used
+// in Focus: https://github.com/dropbox/focus/blob/main/focus-gradle-plugin/settings.gradle.kts
 dependencyResolutionManagement {
-  versionCatalogs {
-    create("libs") {
-      from(files("../gradle/libs.versions.toml"))
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
     }
-  }
 }
