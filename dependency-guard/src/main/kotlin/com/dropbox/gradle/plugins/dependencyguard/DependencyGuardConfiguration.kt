37912b9f54343b90cd232fedf40d3fac968e1e71
Tony Robalik <trobalik@squareup.com>
2022-04-25 00:26:55 -0700

    Use domain object container to avoid afterEvaluate.

--- dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/DependencyGuardConfiguration.kt
+++ dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/DependencyGuardConfiguration.kt
@@ -1,29 +1,45 @@
 package com.dropbox.gradle.plugins.dependencyguard
 
+import org.gradle.api.Named
+import org.gradle.api.tasks.Input
+import javax.inject.Inject
+
 /**
  * Configuration for [DependencyGuardPlugin]
  */
-public class DependencyGuardConfiguration(
+public open class DependencyGuardConfiguration @Inject constructor(
     /**
      * Name of the build configuration
      *
      * See all possibilities by running Gradle's built in ./gradlew project:dependencies
      */
+    @get:Input
     public val configurationName: String,
+) : Named {
+    @Input
+    public override fun getName(): String = configurationName
 
     /** Whether to include artifacts in the dependency list report */
-    public var artifacts: Boolean = true,
+    @get:Input
+    public var artifacts: Boolean = true
 
     /** Whether to include modules in the dependency list report */
-    public var modules: Boolean = true,
+    @get:Input
+    public var modules: Boolean = true
 
     /**
      * Whether to create the dependency tree report
      *
      * false by default because while valuable for debugging, get very noisy
      */
-    public var tree: Boolean = false,
+    @get:Input
+    public var tree: Boolean = false
 
-    /** Whether or not to allow a dependency */
-    public var isAllowed: (dependencyName: String) -> Boolean = { true },
-)
+    /**
+     * Whether to allow a dependency.
+     *
+     * TODO: not sure how to model this as a task input. May not matter since the task that uses it
+     *  can never be up-to-date.
+     */
+    public var isAllowed: (dependencyName: String) -> Boolean = { true }
+}
--- dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/DependencyGuardPlugin.kt
+++ dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/DependencyGuardPlugin.kt
@@ -1,6 +1,5 @@
 package com.dropbox.gradle.plugins.dependencyguard
 
-import com.dropbox.gradle.plugins.dependencyguard.internal.ConfigurationValidators.requirePluginConfig
 import com.dropbox.gradle.plugins.dependencyguard.internal.DependencyTreeDiffTaskNames
 import com.dropbox.gradle.plugins.dependencyguard.internal.list.DependencyGuardListTask
 import com.dropbox.gradle.plugins.dependencyguard.internal.tree.BuildEnvironmentDependencyTreeDiffTask
@@ -8,6 +7,7 @@ import com.dropbox.gradle.plugins.dependencyguard.internal.tree.DependencyTreeDi
 import com.dropbox.gradle.plugins.dependencyguard.internal.isRootProject
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
 
 /**
  * A plugin for watching dependency changes
@@ -25,106 +25,99 @@ public class DependencyGuardPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         val extension = target.extensions.create(
             DEPENDENCY_GUARD_TASK_NAME,
-            DependencyGuardPluginExtension::class.java
+            DependencyGuardPluginExtension::class.java,
+            target.objects
         )
 
-        registerTreeDiffTasks(target, extension)
-        registerDependencyGuardBaselineTask(target, extension)
-        registerDependencyGuardTask(target, extension)
+        val baselineTask = registerDependencyGuardBaselineTask(target, extension)
+        val guardTask = registerDependencyGuardTask(target, extension)
+        registerTreeDiffTasks(
+            target = target,
+            extension = extension,
+            baselineTask = baselineTask,
+            guardTask = guardTask
+        )
     }
 
     private fun registerDependencyGuardBaselineTask(
         target: Project,
         extension: DependencyGuardPluginExtension
-    ) {
-        target.tasks.register(
+    ): TaskProvider<DependencyGuardListTask> {
+        return target.tasks.register(
             DEPENDENCY_GUARD_BASELINE_TASK_NAME,
             DependencyGuardListTask::class.java
         ) {
             val task = this
             task.setParams(
+                project = target,
                 extension = extension,
                 shouldBaseline = true
             )
-            extension.configurations.forEach {
-                // Trigger the dependency tree diff task when treeReport is enabled
-                if (it.tree) {
-                    task.dependsOn(
-                        DependencyTreeDiffTaskNames.createDependencyTreeBaselineTaskNameForConfiguration(
-                            configurationName = it.configurationName
-                        )
-                    )
-                }
-            }
         }
     }
 
     private fun registerDependencyGuardTask(
         target: Project,
         extension: DependencyGuardPluginExtension
-    ) {
-        target.tasks.register(
+    ): TaskProvider<DependencyGuardListTask> {
+        return target.tasks.register(
             DEPENDENCY_GUARD_TASK_NAME,
             DependencyGuardListTask::class.java
         ) {
             val task = this
-            requirePluginConfig(target, extension)
             task.setParams(
+                project = target,
                 extension = extension,
                 shouldBaseline = false
             )
-
-            extension.configurations.forEach {
-                // Trigger the dependency tree diff task when treeReport is enabled
-                if (it.tree) {
-                    task.dependsOn(
-                        DependencyTreeDiffTaskNames.createDependencyTreeTaskNameForConfiguration(
-                            configurationName = it.configurationName
-                        )
-                    )
-                }
-            }
         }
     }
 
-    private fun registerTreeDiffTasks(target: Project, extension: DependencyGuardPluginExtension) {
-        // We must evaluate first so we have the extension data and all flavor configurations are recognized
-        target.afterEvaluate {
-            requirePluginConfig(target, extension)
-            extension.configurations
-                .filter { it.tree } // Only when treeReport is enabled
-                .forEach { dependencyGuardConfiguration ->
-
-                    // Register dependencyTreeDiff tasks for configuration
-                    val configurationName = dependencyGuardConfiguration.configurationName
-                    val taskClass = if (target.isRootProject()) {
-                        BuildEnvironmentDependencyTreeDiffTask::class.java
-                    } else {
-                        DependencyTreeDiffTask::class.java
-                    }
-                    target.tasks.register(
-                        DependencyTreeDiffTaskNames.createDependencyTreeTaskNameForConfiguration(
-                            configurationName = configurationName
-                        ),
-                        taskClass
-                    ) {
-                        setParams(
-                            configurationName = configurationName,
-                            shouldBaseline = false
-                        )
-                    }
-                    target.tasks.register(
-                        DependencyTreeDiffTaskNames.createDependencyTreeBaselineTaskNameForConfiguration(
-                            configurationName = configurationName
-                        ),
-                        taskClass
-                    ) {
-                        setParams(
-                            configurationName = configurationName,
-                            shouldBaseline = true
-                        )
-                    }
+    private fun registerTreeDiffTasks(
+        target: Project,
+        extension: DependencyGuardPluginExtension,
+        baselineTask: TaskProvider<DependencyGuardListTask>,
+        guardTask: TaskProvider<DependencyGuardListTask>
+    ) {
+        extension.configurations.all {
+            val dependencyGuardConfiguration = this
+            if (dependencyGuardConfiguration.tree) {
+                val taskClass = if (target.isRootProject()) {
+                    BuildEnvironmentDependencyTreeDiffTask::class.java
+                } else {
+                    DependencyTreeDiffTask::class.java
                 }
+
+                val treeGuardTask = target.tasks.register(
+                    DependencyTreeDiffTaskNames.createDependencyTreeTaskNameForConfiguration(
+                        configurationName = dependencyGuardConfiguration.configurationName
+                    ),
+                    taskClass
+                ) {
+                    setParams(
+                        configurationName = dependencyGuardConfiguration.configurationName,
+                        shouldBaseline = false
+                    )
+                }
+                guardTask.configure {
+                    dependsOn(treeGuardTask)
+                }
+
+                val treeBaselineTask = target.tasks.register(
+                    DependencyTreeDiffTaskNames.createDependencyTreeBaselineTaskNameForConfiguration(
+                        configurationName = dependencyGuardConfiguration.configurationName
+                    ),
+                    taskClass
+                ) {
+                    setParams(
+                        configurationName = dependencyGuardConfiguration.configurationName,
+                        shouldBaseline = true
+                    )
+                }
+                baselineTask.configure {
+                    dependsOn(treeBaselineTask)
+                }
+            }
         }
     }
 }
