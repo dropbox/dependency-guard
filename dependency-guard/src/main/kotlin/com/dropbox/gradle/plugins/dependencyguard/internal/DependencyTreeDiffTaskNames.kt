1497fe40db7451a082c9d05d65b831b0c90004ab
Sam Edwards <samedwards@dropbox.com>
2022-06-08 16:37:26 -0400

    Backport of the `String.capitalized()` extension for Pre Gradle 7.4.  Fixes #40 (#41)
    * Fixes #40
    
    * Added unit tests for the port of the capitalization extension function.
    
    * Import cleanup.

--- dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/internal/DependencyTreeDiffTaskNames.kt
+++ dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/internal/DependencyTreeDiffTaskNames.kt
@@ -1,8 +1,25 @@
 package com.dropbox.gradle.plugins.dependencyguard.internal
 
-import org.gradle.configurationcache.extensions.capitalized
-
 internal object DependencyTreeDiffTaskNames {
+
+    /**
+     * This extension [org.gradle.configurationcache.extensions.capitalized]
+     * is not available until 7.4.x, so this is a backport.
+     *
+     * Fixes: https://github.com/dropbox/dependency-guard/issues/40
+     */
+    fun String.capitalized(): String {
+        return if (this.isEmpty()) {
+            ""
+        } else {
+            val firstChar = get(0)
+            if (firstChar.isUpperCase()) {
+                return this
+            } else {
+                firstChar.toUpperCase() + substring(1)
+            }
+        }
+    }
 
     fun createDependencyTreeTaskNameForConfiguration(configurationName: String): String {
         return "dependencyTreeDiff${configurationName.capitalized()}"
---
+++ dependency-guard/src/test/kotlin/com/dropbox/gradle/plugins/dependencyguard/internal/TaskNamesTest.kt
@@ -0,0 +1,22 @@
+package com.dropbox.gradle.plugins.dependencyguard.internal
+
+import com.dropbox.gradle.plugins.dependencyguard.internal.DependencyTreeDiffTaskNames.capitalized
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TaskNamesTest {
+    @Test
+    fun `new library is added`() {
+        listOf(
+            "" to "",
+            "a" to "A",
+            "A" to "A",
+            "aa" to "Aa",
+            "aaa" to "Aaa",
+            "aA" to "AA",
+            "Aa" to "Aa",
+        ).forEach {
+            assertEquals(it.first.capitalized(), it.second)
+        }
+    }
+}
