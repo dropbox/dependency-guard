2fb09927ac720211df9c02e1140d2a43351d4407
Aleksandr Zhdanov <qwert2603@mail.ru>
2022-09-10 22:53:02 +0500

    add test cases

--- dependency-guard/src/gradleTest/kotlin/com/dropbox/gradle/plugins/dependencyguard/PluginTest.kt
+++ dependency-guard/src/gradleTest/kotlin/com/dropbox/gradle/plugins/dependencyguard/PluginTest.kt
@@ -1,10 +1,12 @@
 package com.dropbox.gradle.plugins.dependencyguard
 
 import com.dropbox.gradle.plugins.dependencyguard.fixture.Builder.build
+import com.dropbox.gradle.plugins.dependencyguard.fixture.Builder.buildAndFail
+import com.dropbox.gradle.plugins.dependencyguard.fixture.FullProject
 import com.dropbox.gradle.plugins.dependencyguard.fixture.SimpleProject
-import com.dropbox.gradle.plugins.dependencyguard.util.exists
-import com.dropbox.gradle.plugins.dependencyguard.util.readLines
-import com.dropbox.gradle.plugins.dependencyguard.util.readText
+import com.dropbox.gradle.plugins.dependencyguard.util.assertFileExistsWithContentContains
+import com.dropbox.gradle.plugins.dependencyguard.util.assertFileExistsWithContentEqual
+import com.dropbox.gradle.plugins.dependencyguard.util.replaceText
 import com.google.common.truth.Truth.assertThat
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.util.GradleVersion
@@ -19,7 +21,7 @@ class PluginTest {
             args = arrayOf(":lib:dependencyGuard")
         )
 
-        validateBuild(project, result)
+        validateBaselineBuild(project, result)
     }
 
     @Test
@@ -31,7 +33,7 @@ class PluginTest {
             args = arrayOf(":lib:dependencyGuard", "--configuration-cache")
         )
 
-        validateBuild(project, result)
+        validateBaselineBuild(project, result)
 
         // verify configuration-cache related stuff
         assertThat(result.output)
@@ -41,26 +43,138 @@ class PluginTest {
         // There is also the implicit test that the build succeeded, despite these problems.
     }
 
-    private fun validateBuild(project: SimpleProject, result: BuildResult) {
+    private fun validateBaselineBuild(project: SimpleProject, result: BuildResult) {
         // verify baseline
         assertThat(result.output)
             .contains("Dependency Guard baseline created for :lib for configuration compileClasspath.")
 
-        val baseline = project.projectFile("lib/dependencies/compileClasspath.txt")
-        assertThat(baseline.exists()).isTrue()
-
-        val lines = baseline.readLines()
-        assertThat(lines).hasSize(1)
-        assertThat(lines.single()).isEqualTo("commons-io:commons-io:2.11.0")
+        project.assertFileExistsWithContentEqual(
+            filename = "lib/dependencies/compileClasspath.txt",
+            contentFile = "list_before_update.txt",
+        )
 
         // verify tree baseline
-        val treeBaseline = project.projectFile("lib/dependencies/compileClasspath.tree.txt")
-        assertThat(treeBaseline.exists()).isTrue()
-        assertThat(treeBaseline.readText()).contains(
-            """
-              compileClasspath - Compile classpath for source set 'main'.
-              \--- commons-io:commons-io:2.11.0
-            """.trimIndent()
+        project.assertFileExistsWithContentContains(
+            filename = "lib/dependencies/compileClasspath.tree.txt",
+            contentFile = "tree_before_update.txt",
         )
+    }
+
+    @Test
+    fun `guard with no dependencies changes`(): Unit = SimpleProject().use { project ->
+        // create baseline
+        build(
+            project = project,
+            args = arrayOf(":lib:dependencyGuard")
+        )
+
+        // check with no dependencies changes
+        val result = build(
+            project = project,
+            args = arrayOf(":lib:dependencyGuard")
+        )
+
+        assertThat(result.output)
+            .contains("No Dependency Changes Found in :lib for configuration \"compileClasspath\"")
+
+        project.assertFileExistsWithContentEqual(
+            filename = "lib/dependencies/compileClasspath.txt",
+            contentFile = "list_before_update.txt",
+        )
+
+        project.assertFileExistsWithContentContains(
+            filename = "lib/dependencies/compileClasspath.tree.txt",
+            contentFile = "tree_before_update.txt",
+        )
+    }
+
+    @Test
+    fun `guard after dependencies changes`(): Unit = SimpleProject().use { project ->
+        // create baseline
+        build(
+            project = project,
+            args = arrayOf(":lib:dependencyGuard")
+        )
+
+        project.lib.resolve("build.gradle").replaceText(
+            oldValue = "implementation 'io.reactivex.rxjava3:rxjava:3.1.4'",
+            newValue = "implementation 'io.reactivex.rxjava3:rxjava:3.1.5'",
+        )
+
+        // check after dependencies changes
+        val result = buildAndFail(
+            project = project,
+            args = arrayOf(":lib:dependencyGuard")
+        )
+
+        assertThat(result.output)
+            .contains(
+                """
+                > Dependencies Changed in :lib for configuration compileClasspath
+                  - io.reactivex.rxjava3:rxjava:3.1.4
+                  + io.reactivex.rxjava3:rxjava:3.1.5
+                  - org.reactivestreams:reactive-streams:1.0.3
+                  + org.reactivestreams:reactive-streams:1.0.4
+                  
+                  If this is intentional, re-baseline using ./gradlew :lib:dependencyGuardBaseline
+            """.trimIndent()
+            )
+
+        project.assertFileExistsWithContentEqual(
+            filename = "lib/dependencies/compileClasspath.txt",
+            contentFile = "list_before_update.txt",
+        )
+
+        project.assertFileExistsWithContentContains(
+            filename = "lib/dependencies/compileClasspath.tree.txt",
+            contentFile = "tree_before_update.txt",
+        )
+    }
+
+    @Test
+    fun `baseline after dependencies changes`(): Unit = SimpleProject().use { project ->
+        // create baseline
+        build(
+            project = project,
+            args = arrayOf(":lib:dependencyGuard")
+        )
+
+        project.lib.resolve("build.gradle").replaceText(
+            oldValue = "implementation 'io.reactivex.rxjava3:rxjava:3.1.4'",
+            newValue = "implementation 'io.reactivex.rxjava3:rxjava:3.1.5'",
+        )
+
+        // check after dependencies changes
+        val result = build(
+            project = project,
+            args = arrayOf(":lib:dependencyGuardBaseline")
+        )
+
+        assertThat(result.output)
+            .contains("Dependency Guard baseline created for :lib for configuration compileClasspath")
+
+        project.assertFileExistsWithContentEqual(
+            filename = "lib/dependencies/compileClasspath.txt",
+            contentFile = "list_after_update.txt",
+        )
+
+        project.assertFileExistsWithContentContains(
+            filename = "lib/dependencies/compileClasspath.tree.txt",
+            contentFile = "tree_after_update.txt",
+        )
+    }
+
+    @Test
+    fun `can generate full report baseline`(): Unit = FullProject().use { project ->
+        val result = build(
+            project = project,
+            args = arrayOf("dependencyGuard")
+        )
+
+        assertThat(result.output).contains("Dependency Guard baseline created for : for configuration classpath.")
+        assertThat(result.output).contains("Dependency Guard baseline created for :lib for configuration compileClasspath.")
+        assertThat(result.output).contains("Dependency Guard baseline created for :lib for configuration testCompileClasspath.")
+        assertThat(result.output).contains("Dependency Guard Tree baseline created for :lib for configuration compileClasspath.")
+        assertThat(result.output).contains("Dependency Guard Tree baseline created for :lib for configuration testCompileClasspath.")
     }
 }
