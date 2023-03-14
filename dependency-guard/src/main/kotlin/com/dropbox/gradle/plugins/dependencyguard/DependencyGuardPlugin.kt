3044a50e1830d32f8172513fe0301af16f6c820d
Sam Edwards <samedwards@dropbox.com>
2023-03-14 10:22:36 -0400

    Changed Configuration Validation

--- dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/DependencyGuardPlugin.kt
+++ dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/DependencyGuardPlugin.kt
@@ -1,10 +1,7 @@
 package com.dropbox.gradle.plugins.dependencyguard
 
-import com.dropbox.gradle.plugins.dependencyguard.internal.ConfigurationValidators
 import com.dropbox.gradle.plugins.dependencyguard.internal.DependencyTreeDiffTaskNames
-import com.dropbox.gradle.plugins.dependencyguard.internal.isRootProject
 import com.dropbox.gradle.plugins.dependencyguard.internal.list.DependencyGuardListTask
-import com.dropbox.gradle.plugins.dependencyguard.internal.projectConfigurations
 import com.dropbox.gradle.plugins.dependencyguard.internal.tree.DependencyTreeDiffTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -46,33 +43,6 @@ public class DependencyGuardPlugin : Plugin<Project> {
             target = target,
             dependencyGuardTask = dependencyGuardTask
         )
-
-        validateConfigurationAfterEvaluate(target, extension)
-    }
-
-    /**
-     * It's recommended to avoid `afterEvaluate` but in this case we are doing a validation of inputs
-     *
-     * Without adding this, there were cases when the `target.configurations` were not all available yet.
-     */
-    private fun validateConfigurationAfterEvaluate(target: Project, extension: DependencyGuardPluginExtension) {
-        target.afterEvaluate {
-            val availableConfigurationNames = target.configurations.map { it.name }
-            val monitoredConfigurationNames = extension.configurations.map { it.configurationName }
-            ConfigurationValidators.requirePluginConfig(
-                projectPath = target.path,
-                isForRootProject = target.isRootProject(),
-                availableConfigurations = availableConfigurationNames,
-                monitoredConfigurations = monitoredConfigurationNames,
-            )
-            ConfigurationValidators.validateConfigurationsAreAvailable(
-                projectPath = target.path,
-                isForRootProject = target.isRootProject(),
-                logger = target.logger,
-                availableConfigurationNames = availableConfigurationNames,
-                monitoredConfigurationNames = monitoredConfigurationNames
-            )
-        }
     }
 
     private fun attachToCheckTask(target: Project, dependencyGuardTask: TaskProvider<DependencyGuardListTask>) {
@@ -110,8 +80,7 @@ public class DependencyGuardPlugin : Plugin<Project> {
             DEPENDENCY_GUARD_TASK_NAME,
             DependencyGuardListTask::class.java
         ) {
-            val task = this
-            task.setParams(
+            setParams(
                 project = target,
                 extension = extension,
                 shouldBaseline = false
--- dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/internal/tree/DependencyTreeDiffTask.kt
+++ dependency-guard/src/main/kotlin/com/dropbox/gradle/plugins/dependencyguard/internal/tree/DependencyTreeDiffTask.kt
@@ -8,6 +8,7 @@ import com.dropbox.gradle.plugins.dependencyguard.internal.utils.OutputFileUtils
 import com.dropbox.gradle.plugins.dependencyguard.internal.utils.Tasks.declareCompatibilities
 import java.io.File
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.artifacts.result.ResolvedComponentResult
 import org.gradle.api.provider.Property
@@ -46,10 +47,22 @@ internal abstract class DependencyTreeDiffTask : DefaultTask() {
         shouldBaseline: Boolean,
     ) {
         val projectDependenciesDir = OutputFileUtils.projectDirDependenciesDir(project)
-        val projectDirOutputFile: File = DependencyGuardTreeDiffer.projectDirOutputFile(projectDependenciesDir, configurationName)
-        val buildDirOutputFile: File = DependencyGuardTreeDiffer.buildDirOutputFile(project.layout.buildDirectory.get(), configurationName)
+        val projectDirOutputFile: File = DependencyGuardTreeDiffer.projectDirOutputFile(
+            projectDirectory = projectDependenciesDir,
+            configurationName = configurationName
+        )
+        val buildDirOutputFile: File = DependencyGuardTreeDiffer.buildDirOutputFile(
+            buildDirectory = project.layout.buildDirectory.get(),
+            configurationName = configurationName
+        )
         val projectPath = project.path
-        val resolvedComponentResult = project.projectConfigurations.getResolvedComponentResult(configurationName)
+
+        val resolvedComponentResult: Provider<ResolvedComponentResult> =
+            project.projectConfigurations.getResolvedComponentResult(configurationName)
+                ?: throw GradleException(buildString {
+                    appendLine("Dependency Guard could not resolve configuration $configurationName for $projectPath.")
+                    appendLine("Please update your configuration and try again.")
+                })
 
         this.resolvedComponentResult.set(resolvedComponentResult)
         this.projectDirOutputFile.set(projectDirOutputFile)
